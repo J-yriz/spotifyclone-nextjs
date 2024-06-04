b3beaa763f119263c511b0d883aa83d05c3da758
Jyriz <discordketukangan123@gmail.com>
2024-06-04 07:23:19 +0700

    fixedDevRe-Render

--- src/app/(site)/component/getToken.jsx
+++ src/app/(site)/component/getToken.jsx
@@ -1,17 +1,20 @@
 import Cryptr from "cryptr";
 import Cookies from "js-cookie";
-import { useState } from "react";
 import { useRouter } from "next/navigation";
+import { useEffect, useRef } from "react";
 
 import config from "@/config";
 const { client_id, client_secret, redirect_uri, lavalink: { host, port } } = config;
 
 export default function GetToken({ code }) {
     const router = useRouter();
-    const [cancel, setCancel] = useState(false);
     const cryptr = new Cryptr('myTotallySecretKey', { encoding: 'base64', pbkdf2Iterations: 10000, saltLength: 20 });
+    const diGunakan = useRef(false);
     const getToken = async () => {
-        if (cancel === false) {
+        if (diGunakan.current) return;
+        diGunakan.current = true;
+
+        try {
             const response = await fetch(`${host}${port}/callback`, {
                 method: "POST",
                 headers: {
@@ -25,18 +28,24 @@ export default function GetToken({ code }) {
                 }),
             });
             const data = await response.json();
-            console.log(data);
-            // const tokened = data;
-            // const token = Cookies.get("token");
-            // if (!token) {
-            //     Cookies.set("token", cryptr.encrypt(tokened.access_token), { expires: 1 / 24 });
-            // } else {
-            //     Cookies.remove("token");
-            //     Cookies.set("token", cryptr.encrypt(tokened.access_token), { expires: 1 / 24 });
-            // }
-            // router.push("/spotify");
-            setCancel(true)
+            const token = Cookies.get("token");
+            if (!token) {
+                Cookies.set("token", cryptr.encrypt(data.access_token), { expires: 1 / 24 });
+            } else {
+                Cookies.remove("token");
+                Cookies.set("token", cryptr.encrypt(data.access_token), { expires: 1 / 24 });
+            }
+            router.push("/spotify");
+        } catch (error) {
+            console.error("Failed to get access token:", error);
+        } finally {
+            diGunakan.current = false;
         }
-    };
-    getToken();
+    }
+    useEffect(() => {
+        let isMounted = true;
+        console.log(isMounted);
+        if (isMounted && !diGunakan.current) getToken();
+        return () => isMounted = false;
+    }, []);
 }
