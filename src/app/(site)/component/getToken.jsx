7059cfb3c18b8709a9ebe2000710ea25df7db635
Jyriz <discordketukangan123@gmail.com>
2024-06-07 10:44:51 +0700

    updateFetchEndpoint

--- src/app/(site)/component/getToken.jsx
+++ src/app/(site)/component/getToken.jsx
@@ -1,39 +1,35 @@
-import Cryptr from "cryptr";
 import Cookies from "js-cookie";
 import { useRouter } from "next/navigation";
 import { useEffect, useRef } from "react";
 
 import config from "@/config";
-const { client_id, client_secret } = config;
+const { client_id, client_secret, redirect_uri, lavalink: { host, port } } = config;
 
 export default function GetToken() {
     const router = useRouter();
     const diGunakan = useRef(false);
-    const cryptr = new Cryptr('myTotallySecretKey', { encoding: 'base64', pbkdf2Iterations: 10000, saltLength: 20 });
     const getToken = async () => {
         if (diGunakan.current) return;
         diGunakan.current = true;
 
         try {
-            if ( !client_id || !client_secret ) throw new Error("Client ID or Client Secret is not defined");
-            const response = await fetch(`https://accounts.spotify.com/api/token`, {
+            const response = await fetch(`${host}${port}/callback`, {
                 method: "POST",
                 headers: {
-                    "Content-Type": "application/x-www-form-urlencoded",
+                    "Content-Type": "application/json"
                 },
-                body: new URLSearchParams({
-                    grant_type: "client_credentials",
+                body: JSON.stringify({
                     client_id,
                     client_secret
                 }),
             });
-            const { access_token } = await response.json();
+            const data = await response.json();
             const token = Cookies.get("token");
             if (!token) {
-                Cookies.set("token", cryptr.encrypt(access_token), { expires: 1 / 24 });
+                Cookies.set("token", data, { expires: 1 / 24 });
             } else {
                 Cookies.remove("token");
-                Cookies.set("token", cryptr.encrypt(access_token), { expires: 1 / 24 });
+                Cookies.set("token", data, { expires: 1 / 24 });
             }
             router.push("/spotify");
         } catch (error) {
